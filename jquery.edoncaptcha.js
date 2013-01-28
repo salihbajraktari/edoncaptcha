a8c45cf80dfad634d6d3ba88889066f594957bb1
DonDi1989 <edonbajrami89@gmail.com>
2013-01-28 15:35:18 -0800

    Create jquery.edoncaptcha.js

---
+++ jquery.edoncaptcha.js
@@ -0,0 +1,41 @@
+(function($){
+
+  jQuery.fn.edoncaptcha = function(options){
+
+		var element = this; 
+		var submit = $(this).find('input[type=submit]');
+		$('<label id="edoncaptchatext"></label>').insertBefore(submit);
+		$('<input type="text" class="textbox" id="edoncaptchainput"/><br/><br/>').insertBefore(submit);
+		var input = this.find('#edoncaptchainput'); 
+		var label = this.find('#edoncaptchatext'); 
+		
+		$(element).find('input[type=submit]').attr('disabled','disabled'); 
+
+		// Initialization of variables
+		var randomNr1 = 0; 
+		var randomNr2 = 0;
+		var totalNr = 0;
+
+		randomNr1 = Math.floor(Math.random()*10);
+		randomNr2 = Math.floor(Math.random()*10);
+		totalNr = randomNr1 + randomNr2;
+		var texti = "What is "+randomNr1+" + "+randomNr2;
+		$(label).text(texti);
+		
+	
+		$(input).keyup(function(){
+
+			var nr = $(this).val();
+			if(nr==totalNr)
+			{
+				$(element).find('input[type=submit]').removeAttr('disabled');				
+			}
+			else{
+				$(element).find('input[type=submit]').attr('disabled','disabled');
+			}
+			
+		});
+
+	};
+
+})(jQuery);
